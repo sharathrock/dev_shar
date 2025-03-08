8b8704c3292547a92ede3320058fca9e3ba29f9d
sharathrock <sharathrock@gmail.com>
2025-03-08 20:15:50 +0530

    Branch2 (#3)
    * branch add
    
    * third commit
    
    * merge practice branch2 with branch3
    
    ---------
    
    Co-authored-by: sharath tester <sharathkumarsn@gmail.com>

--- myfile.sh
+++ myfile.sh
@@ -1,1 +1,5 @@
+branch2
+test to confirm the merge branch1 with branch2 
+=======
 test to check the branch merge
+
--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
-<<<<<<< HEAD
+
+This is my first script and i am testing now
+added changes
+=======
+
 Your local changes
 =======
 Remote changes from origin/main
->>>>>>> origin/main
+
+
 
